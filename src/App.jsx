15ba7998322029623b96cc602274a7c34f87fed8
Luca-Orabona <lucaorabona94@gmail.com>
2025-08-17 18:16:56 +0200

    feat: product list created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 
 function App() {
+  const products = [
+    { name: 'Mela', price: 0.5 },
+    { name: 'Pane', price: 1.2 },
+    { name: 'Latte', price: 1.0 },
+    { name: 'Pasta', price: 0.7 },
+  ];
 
+  
   return (
     <>
-     
+      <ul>
+        {products.map((p, i) => (
+          <li key={i} className="flex gp mb-10">
+            <h2>{p.name}: </h2>
+            <span>{p.price.toFixed(2)}€</span>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
