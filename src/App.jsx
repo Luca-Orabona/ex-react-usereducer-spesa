837dce651c25f65d236e1d7f832f2ec63be7473a
Luca-Orabona <lucaorabona94@gmail.com>
2025-08-17 19:04:46 +0200

    feat: added products to cart completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
 
 function App() {
+
+  const [addedProducts, setAddedProducts] = useState([]);
+
+
   const products = [
     { name: 'Mela', price: 0.5 },
     { name: 'Pane', price: 1.2 },
@@ -7,7 +12,15 @@ function App() {
     { name: 'Pasta', price: 0.7 },
   ];
 
-  
+  const addToCart = (product) => {
+    const productToAdd = addedProducts.find(p => p.name === product.name);
+    if (!productToAdd) {
+      setAddedProducts(curr => [...curr, { ...product, quantity: 1 }])
+    }
+  };
+  console.log(addedProducts);
+
+
   return (
     <>
       <ul>
@@ -15,9 +28,24 @@ function App() {
           <li key={i} className="flex gp mb-10">
             <h2>{p.name}: </h2>
             <span>{p.price.toFixed(2)}€</span>
+            <button className="ms-15" onClick={() => addToCart(p)}>Aggiungi al carrello</button>
           </li>
         ))}
       </ul>
+
+      {addedProducts.length > 0 &&
+        <ul>
+          <li className="text-center"><h1>Carrello</h1></li>
+          {addedProducts.map((p, i) => (
+            <li key={i} className="bg-green p-10 mb-10">
+              <div className="flex gp mb-10">
+                <h2>{p.name}: </h2>
+                <span>{p.price.toFixed(2)}€</span>
+              </div>
+              <p>Quantità: {p.quantity}</p>
+            </li>
+          ))}
+        </ul>}
     </>
   )
 }
